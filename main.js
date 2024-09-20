6820aeed5cb25abb0bde1a922c7492b9ecc2c75f
HenriqueDanilo1 <74213860+HenriqueDanilo1@users.noreply.github.com>
2024-09-20 16:47:22 -0300

    gg

--- main.js
+++ main.js
@@ -1,59 +1,72 @@
-const buser = localStorage.getItem('local-user')
-const bpass = localStorage.getItem('local-pass')
-const admuser = "pobre"
-const admpass = "1234"
+//test database
+let database_user = [ "pobre", "rico", "probre pro max" ]
+let database_pass = [ "1234", "onetwothreefour", "Porta01!" ]
 
+//bank name
+bn = "BL Bank"
+
+//validate system
+let validate = (user, pass) => {
+    for(i = 0; i < database_user.length; i++){
+        if(database_user[i] == user){
+            if(database_pass[i] == pass){
+                return 1
+            } else {
+                return -1
+            }
+        }
+    }
+    return 0
+}
+
+//alert system]
+function falert(n){
+    arlert = document.getElementById("alert")
+    if(n == 0){
+        n = "Password incorrect!"
+    } else{
+        n = "User not found"
+    }
+    arlert.innerHTML=n
+    arlert.style.display="block"
+    document.getElementById("arrow").style.top="306px"
+}
+
+//submit function
 function submit(){
     const user = document.querySelector('#user').value
     const pass = document.querySelector('#pass').value
-    if(user == admuser && pass == admpass){
-        alert('adm')
-    } else if(buser == user && bpass == pass){
+    if(validate(user, pass) == 1){
         document.location.href="gg.html"
-    } else if(buser != user){
-        alert('Usuario não identificado!')
+    } else if(validate(user, pass) == -1){
+        falert(0) // 0 = pass
     } else{
-        alert('Senha incorreta!')
-    } 
-}
-function create(){
-    const admuser = "pobre"
-    alert("I'll make 4 questions for you okay?")
-    alert("Oh right, that was the first one, lets for the second one")
-    const buser = prompt('What will your nick be?')
-    if(buser != admuser){
-        alert(`Your nick is ${buser}`)
-        const bpass = prompt("Write your password here. NO. THIS NOT A QUESTION HAAHHAAHAH")
-        const bpass2 = prompt('Now write again hahaahahhahahaha')
-        if(bpass === bpass2){
-            localStorage.setItem('local-pass', bpass)
-            localStorage.setItem('local-user', buser)
-            alert('Wait a second please, the system is very slow.')
-            alert('Wait a second please, the system is very slow.')
-            alert('Wait a second please, the system is very slow.')
-            alert('Wait a second please, the system is very slow.')
-            alert('Wait a second please, the system is very slow.')
-            alert('Wait a second please, the system is very slow.')
-            alert('Wait a second please, the system is very slow.')
-            alert('Wait a second please, the system is very slow.')
-            alert('Wait a second please, the system is very slow.')
-            alert('Wait a second please, the system is very slow.')
-            alert('Wait a second please, the system is very slow.')
-            alert('Wait a second please, the system is very slow.')
-            alert('Wait a second please, the system is very slow.')
-            alert('Wait a second please, the system is very slow.')
-            alert('Wait a second please, the system is very slow.')
-            alert('Your account is ready for use.')
-            location.reload()
-        } else{
-            alert('The passwords dont match, please try again.')
-        }
-    } else {
-        alert('This username already exists. Please try again.')
+        falert(1) // 1 = user
     }
 }
+function create(){
+    let body = document.getElementsByTagName("body")[0]
+    let ui = document.getElementById("create")
+    //add blur and complete ui
+    ui.style.transform="translate(-50%, -50%) scale(1)"
+    // const newuser = prompt("First create your username:")
+    // if(newuser == null){return}
+    // if(validate(newuser) == 0){
+    //     const newpass_1 = prompt("Now create your password: ")
+    //     if(newpass_1 == null){return}
+    //     const newpass_2 = prompt("Finally repeat your password: ")
+    //     if(newpass_1 == newpass_2){
+    //         database_user.push(newuser)
+    //         database_pass.push(newpass_1)
+    //         alert(`Wellcome to ${bn}, it's great to have you with us!`)
+    //     } else{
+    //         alert("Oh no! The passwords are different, please try again!")
+    //     }
+    // } else{
+    //     alert("Oh no! This user already exist. Please try again.")
+    //     create()
+    // }
+    }
 function forget(){
-    prompt('Write  a text to describe your problem.')
-    let name = prompt('What your name?')
-    alert(`BUT I DON'T CARE ${name} KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK`)
+    alert("I'm really sorry but WE DON'T FREAKING CARE!")
 }
