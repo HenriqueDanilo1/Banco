b58559116011e83f02b24f17a1a01315c117b13f
HenriqueDanilo1 <74213860+HenriqueDanilo1@users.noreply.github.com>
2024-09-21 13:05:47 -0300

    aaa

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let database_pass = [ "1234", "onetwothreefour", "Porta01!" ]
 //bank name
 bn = "BL Bank"
 
-//validate system
+//login system
 let validate = (user, pass) => {
     for(i = 0; i < database_user.length; i++){
         if(database_user[i] == user){
@@ -19,7 +19,7 @@ let validate = (user, pass) => {
     return 0
 }
 
-//alert system]
+//alert system
 function falert(n){
     arlert = document.getElementById("alert")
     if(n == 0){
@@ -32,41 +32,71 @@ function falert(n){
     document.getElementById("arrow").style.top="306px"
 }
 
-//submit function
+//login ui function
 function submit(){
     const user = document.querySelector('#user').value
     const pass = document.querySelector('#pass').value
     if(validate(user, pass) == 1){
         document.location.href="gg.html"
-    } else if(validate(user, pass) == -1){
-        falert(0) // 0 = pass
     } else{
-        falert(1) // 1 = user
+        (validate(user, pass) == -1) ? falert(0) /* wrong password*/ : falert(1) // wrong username
     }
 }
+
+//pass verifier for internal functions
+function passverifier(pass1, pass2) {
+    if(pass1 == pass2) {
+        if(pass1.length >= 8) {
+            let n = /[0-9]/
+            let char = /[!@#$%&*|?:;=+\-\...]/
+            if(n.test(pass1) == true && char.test(pass1) == true) {
+                return 0
+            } else {return 3}
+        } else {return 2}
+    } else {return 1}
+}
+
+//creat an acc function
+function register(){
+    //get data with DOM manipulation
+    let c_user = document.getElementById("c_user").value 
+    //let c_email = document.getElementById("c_email").value no one care for emails THIS IS MY BANK AND I DO WHAT I WANT
+    let c_pass1 = document.getElementById("c_pass1").value 
+    let c_pass2 = document.getElementById("c_pass2").value
+    let c_alert = document.getElementById("c_alert")
+    if(validate(c_user) == 0) {
+        switch(passverifier(c_pass1, c_pass2)) {
+            case 0: //successful
+                database_user.push(c_user)
+                database_pass.push(c_pass1)
+                c_alert.style.display="block"
+                c_alert.innerHTML=("Register conclude with successful")
+                break;
+            case 1: //pass are different
+                c_alert.style.display="block"
+                c_alert.innerHTML=("Passwords are different")
+                break;
+            case 2: //pass too short
+                c_alert.style.display="block"
+                c_alert.innerHTML=("Password too short")
+                break;
+            case 3: //pass is too easy
+                c_alert.style.display="block"
+                c_alert.innerHTML=("Password doesn't meet minimum requirements")
+        }
+    } else {
+        c_alert.innerHTML="User already exists"
+    }
+}
+
+//create button - isnt working
 function create(){
     let body = document.getElementsByTagName("body")[0]
     let ui = document.getElementById("create")
     //add blur and complete ui
     ui.style.transform="translate(-50%, -50%) scale(1)"
-    // const newuser = prompt("First create your username:")
-    // if(newuser == null){return}
-    // if(validate(newuser) == 0){
-    //     const newpass_1 = prompt("Now create your password: ")
-    //     if(newpass_1 == null){return}
-    //     const newpass_2 = prompt("Finally repeat your password: ")
-    //     if(newpass_1 == newpass_2){
-    //         database_user.push(newuser)
-    //         database_pass.push(newpass_1)
-    //         alert(`Wellcome to ${bn}, it's great to have you with us!`)
-    //     } else{
-    //         alert("Oh no! The passwords are different, please try again!")
-    //     }
-    // } else{
-    //     alert("Oh no! This user already exist. Please try again.")
-    //     create()
-    // }
-    }
+}
+//i really dont care for this function, my bad
 function forget(){
     alert("I'm really sorry but WE DON'T FREAKING CARE!")
 }
